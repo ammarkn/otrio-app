dcd2f9e5e1f077042b93bcb1cc84f4290fadda0c
Ammar <ammar.k@dal.ca>
2023-03-08 17:15:16 -0400

    Added functionality to buttons in board activity page

--- app/src/main/java/com/example/otrio/BoardActivity.kt
+++ app/src/main/java/com/example/otrio/BoardActivity.kt
@@ -1,11 +1,27 @@
 package com.example.otrio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class BoardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board)
+
+        val homeButtonClick = findViewById<Button>(R.id.homeButton)
+        homeButtonClick.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val instructionsButtonClick = findViewById<Button>(R.id.instructionsButton)
+        instructionsButtonClick.setOnClickListener {
+            val intent = Intent(this, InstructionsActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+
     }
 }
